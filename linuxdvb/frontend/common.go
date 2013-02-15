1400ba3f7f7fddda2c11af6b35e514360be24bf6
Michał Derkacz <michal@Lnet.pl>
2013-02-15 16:50:54 +0100

    SecParam added

--- linuxdvb/frontend/common.go
+++ linuxdvb/frontend/common.go
@@ -16,3 +16,24 @@ const (
 	ToneOn Tone = iota
 	TonefOff
 )
+
+// SecParam calculates intermediate frequency, tone and voltage for given
+// absolute frequency (HZ) and polarization ('h' or 'v').
+func SecParam(freq uint64, polarization byte) (f uint64, t Tone, v Voltage) {
+	switch polarization {
+	case 'h':
+		v = Voltage18
+	case 'v':
+		v = Voltage13
+	default:
+		panic("unknown polarization")
+	}
+	if freq < 11700000 {
+		f = freq - 9750000
+		t = TonefOff
+	} else {
+		f = freq - 10600000
+		t = TonefOn
+	}
+	return
+}
