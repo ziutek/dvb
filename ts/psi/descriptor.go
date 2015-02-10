4105f20a10cc7995e20cd9dd881c1b0173475fda
Michał Derkacz <michal@Lnet.pl>
2015-02-10 18:34:13 +0100

    psi: More descriptor tags

--- ts/psi/descriptor.go
+++ ts/psi/descriptor.go
@@ -38,11 +38,36 @@ func (dl DescriptorList) Append(d Descriptor) DescriptorList {
 type DescriptorTag byte
 
 const (
-	CATag         DescriptorTag = 0x09
-	ISO639LangTag DescriptorTag = 0x0a
+	VideoStreamTag                DescriptorTag = 0x02
+	AudioStreamTag                DescriptorTag = 0x03 //PMT
+	HierarchyTag                  DescriptorTag = 0x04
+	RegistrationTag               DescriptorTag = 0x05 //PMT
+	DataStreamAlignmentTag        DescriptorTag = 0x06
+	TargetBackgroundGridTag       DescriptorTag = 0x07
+	VideoWindowTag                DescriptorTag = 0x08
+	CATag                         DescriptorTag = 0x09
+	ISO639LangTag                 DescriptorTag = 0x0A //PMT
+	SystemClockTag                DescriptorTag = 0x0B
+	MultiplexBufferUtilizationTag DescriptorTag = 0x0C
+	CopyrightTag                  DescriptorTag = 0x0D
+	MaximumBitrateTag             DescriptorTag = 0x0E //PMT
+	PrivateDataIndicatorTag       DescriptorTag = 0x0F
+	SmoothingBufferTag            DescriptorTag = 0x10
+	STDTag                        DescriptorTag = 0x11
+	IBPTag                        DescriptorTag = 0x12
 
-	NetworkNameTag               DescriptorTag = 0x40
-	ServiceListTag               DescriptorTag = 0x41
+	MPEG4VideoTag      DescriptorTag = 0x1b
+	MPEG4AudioTag      DescriptorTag = 0x1c
+	IODTag             DescriptorTag = 0x1d
+	SLTag              DescriptorTag = 0x1f
+	FMCTag             DescriptorTag = 0x20
+	ExternalESIDTag    DescriptorTag = 0x21
+	MuxCodeTag         DescriptorTag = 0x22
+	FmxBufferSizeTag   DescriptorTag = 0x23
+	MultiplexBufferTag DescriptorTag = 0x24
+
+	NetworkNameTag               DescriptorTag = 0x40 //NIT
+	ServiceListTag               DescriptorTag = 0x41 //NIT
 	StuffingTag                  DescriptorTag = 0x42
 	SatelliteDeliverySystemTag   DescriptorTag = 0x43
 	CableDeliverySystemTag       DescriptorTag = 0x44
@@ -57,23 +82,23 @@ const (
 	TimeShiftedEventTag          DescriptorTag = 0x4F
 	ComponentTag                 DescriptorTag = 0x50
 	MosaicTag                    DescriptorTag = 0x51
-	StreamIdentifierTag          DescriptorTag = 0x52
+	StreamIdentifierTag          DescriptorTag = 0x52 //PMT
 	CAIdentifierTag              DescriptorTag = 0x53
 	ContentTag                   DescriptorTag = 0x54
 	ParentalRatingTag            DescriptorTag = 0x55
 	TeletextTag                  DescriptorTag = 0x56
 	TelephoneTag                 DescriptorTag = 0x57
 	LocalTimeOffsetTag           DescriptorTag = 0x58
-	SubtitlingTag                DescriptorTag = 0x59
-	TerrestrialDeliverySystemTag DescriptorTag = 0x5A
+	SubtitlingTag                DescriptorTag = 0x59 //PMT
+	TerrestrialDeliverySystemTag DescriptorTag = 0x5A //NIT
 	MultilingualNetworkName      DescriptorTag = 0x5B
 	MultilingualBouquetName      DescriptorTag = 0x5C
 	MultilingualServiceName      DescriptorTag = 0x5D
 	MultilingualComponent        DescriptorTag = 0x5E
-	PrivateDataSpecifier         DescriptorTag = 0x5F
+	PrivateDataSpecifier         DescriptorTag = 0x5F //NIT
 	ServiceMove                  DescriptorTag = 0x60
 	ShortSmoothingBuffer         DescriptorTag = 0x61
-	FrequencyList                DescriptorTag = 0x62
+	FrequencyList                DescriptorTag = 0x62 //NIT
 	PartialTransportStreamTag    DescriptorTag = 0x63
 	DataBroadcastTag             DescriptorTag = 0x64
 	CASystemTag                  DescriptorTag = 0x65
@@ -81,14 +106,16 @@ const (
 	TransportStreamTag           DescriptorTag = 0x67
 	DSNGTag                      DescriptorTag = 0x68
 	PDCTag                       DescriptorTag = 0x69
-	AC3Tag                       DescriptorTag = 0x6a
+	AC3Tag                       DescriptorTag = 0x6a //PMT
 	AncillaryDataTag             DescriptorTag = 0x6b
 	CellListTag                  DescriptorTag = 0x6c
 	CellFrequencyLinkTag         DescriptorTag = 0x6d
 	AnnouncementSupportTag       DescriptorTag = 0x6e
-	ApplicationSignalling        DescriptorTag = 0x6f
+	ApplicationSignalling        DescriptorTag = 0x6f //PMT
 
-	EnhancedAC3Tag DescriptorTag = 0x7a
+	EnhancedAC3Tag DescriptorTag = 0x7a //PMT
 	DTSTag         DescriptorTag = 0x7b
 	AACTag         DescriptorTag = 0x7c
+
+	LogicalCannelTag DescriptorTag = 0x83 //NIT
 )
